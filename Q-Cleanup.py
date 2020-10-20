25c20b57e5abe6c923612eee105fb1a71e5e6dfa
Simon Schofield <scho15@hotmail.com>
2020-10-20 22:43:57 +0100

    Getting different results to Java - need to establish why.

--- Q-Cleanup.py
+++ Q-Cleanup.py
@@ -3,7 +3,7 @@ import os.path
 from collections import Counter
 
 class QCleanup:
-	# Simple file to read Q and see if any entries exceed 90 (and Q can be "dumped" back in edited form)
+	# Simple file to read Q and see if any entries exceed 88 (and Q can be "dumped" back in edited form)
 	# Could use later to sort through Q and see some interesting properties
 	def cleanser():
 		Q = []
@@ -32,7 +32,6 @@ class QCleanup:
 
 	def massedit(iteration):
 		Q = []
-		a = []	
 		counter = 0
 		if (os.path.isfile('Q-Table.txt') == True):
 			with open("Q-table.txt", "r") as QTablefile:
@@ -42,16 +41,15 @@ class QCleanup:
 			if (len(item[0]) == iteration and item[2] > 0):
 				item[2] = 0
 				counter = counter + 1
-		print(f"Number of items amended to length 0 was {counter}")
-		#with open("Q-table.txt","w") as handler:
-		#	json.dump(Q,handler) 
-		#handler.close()    
+		print(f"Number of items amended to length 0 for iteration {iteration} was {counter}")
+		with open("Q-table.txt","w") as handler:
+			json.dump(Q,handler) 
+		handler.close()    
 
 	def reader(iteration):
 		Q = []
-		a = []	
 		sum = 0
-		minimum = 0
+		minimum = 200
 		QDictLength = {}
 		QDictVisits = {}
 		if (os.path.isfile('Q-Table.txt') == True):
@@ -60,7 +58,7 @@ class QCleanup:
 				print(f"Q-table uploaded with {len(Q)} lines")
 		for item in Q:
 			length = len(item[0])
-			if (length == iteration and item[2] >= minimum):
+			if (length == iteration and item[1] >= minimum):
 				QDictLength[str(item[0])] = item[1] # 1 for iteration and 2 for visit
 				QDictVisits[str(item[0])] = item[2]
 				sum += 1
@@ -70,5 +68,35 @@ class QCleanup:
 		entries = sorted(QDictLength.items(), key=lambda x: x[1], reverse=True)
 		for element in entries:			
 			print(f"{element[0]}, {element[1]}, {QDictVisits[element[0]]}")
-		
-QCleanup.reader(3)
+
+	def updateFrom88():
+		Q = []
+		itemList = []
+		updateList = []
+		textInput = ""
+		#if (os.path.isfile('Q-Table.txt') == True):
+		#	with open("Q-table.txt", "r") as QTablefile:
+		#		Q = json.load(QTablefile)
+		#		print(f"Q-table uploaded with {len(Q)} lines")	
+		textInput = input("Enter the iteration to be updated in Q with brackets: ")
+		textInput = textInput[1:-1]
+		itemList = [int(item) for item in textInput.split(',')]
+		print(itemList)
+		updateList = itemList.copy()[:88]
+		while(len(updateList) > 3):
+			itemFound = False
+			for item in Q:
+				if interimList == item[0]:
+					item[2] = 1
+					itemFound = True
+					break
+			if (itemFound == False):
+				print(f"{updateList} has not been found")
+			updateList.pop()
+		#with open("Q-table.txt","w") as handler:
+		#	json.dump(Q,handler) 
+		#handler.close()    
+
+#QCleanup.cleanser()	
+QCleanup.reader(4)
+#QCleanup.updateFrom88()
