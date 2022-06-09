a30d071665e0c8c2d1008087dddc728f08650bbd
Simon Schofield <scho15@hotmail.com>
2022-06-09 20:28:39 +0100

    First run of 650k

--- EternityStart.py
+++ EternityStart.py
@@ -12,9 +12,9 @@ class EternityStart():
     def main():
         # DECISIONS REQUIRED
         useHints = False # Use only centre tile or 4 corner hints as well
-        maxEpisodes = 103 # number of episodes to run
+        maxEpisodes = 205 # number of episodes to run
         sampleSize = 1 # number of runs/samples to take - 1 for no hints and 2 for hints typically
-        CreateTile.firstCountLimit = 600000 # cutoff for run - normally at least 1m
+        CreateTile.firstCountLimit = 650000 # cutoff for run - normally at least 1m
         CreateTile.terminalCountLimit = 20 * CreateTile.firstCountLimit # cutoff for final iteration at 88
         solutionPrint = 205; # can consider 200,205 or similar
         cutoff = 96 # Point at which we move from sample check to full/5m solution
